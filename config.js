554ca039ca70bd69aab75389db4c40cd28b6b717
adityalendal <86916790+adityalendal@users.noreply.github.com>
2022-02-13 19:03:15 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+export const firebaseConfig = {
+    apiKey: "AIzaSyBhS1Z17PtG3oPp1ganW3OWRxVj9vTkmdA",
+    authDomain: "timetable-43f55.firebaseapp.com",
+    projectId: "timetable-43f55",
+    storageBucket: "timetable-43f55.appspot.com",
+    messagingSenderId: "1093945984754",
+    appId: "1:1093945984754:web:8b61d7d23d15364734f26b",
+    databaseURL:"https://timetable-43f55-default-rtdb.firebaseio.com"
+
+  };
+  firebase.initializeApp(firebaseConfig)
---
+++ screens/homescreen.js
@@ -0,0 +1,14 @@
+/*gmikfdohgjroihghfguifdhgiofshfsduijfghwreduipfhfsdugoihfsdaguoifeytdgguifdoghuioerghjuifdhguoijgfhduoijg ergfheruighretgreg
+ergregehgjdikflghluirghueijoghuijoghjekghejkglhelijgerhg
+e
+ger
+gerger
+gregregregergrsthjytjtdjgj
+rj
+rjryt5j+626526+526263241632134266
+54
+6
+4
+65455
+341
+654165314564123165*/
